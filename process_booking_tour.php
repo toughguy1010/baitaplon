8f1b55b53cc5a2b5145b3b9ca8f642dc976805a8
Minh Quan <90233506+MinhQuanTheQP@users.noreply.github.com>
2021-11-01 00:33:11 +0700

    Add files via upload

---
+++ process_booking_tour.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="css/process_booking_tour.css">
+<?php
+ include('config/header.php')
+?>
+
+<!-- type here -->
+
+
+
+<div class=" ">
+        <h2 class=" d-flex justify-content-center "> BẠN ĐÃ ĐẶT TOUR THÀNH CÔNG </h2>
+
+    <div class ="container">
+        <div class="">
+                <a href="detail.php">
+                    <button type="submit" class="btn-1">Xem chi tiết</button>
+                </a>
+                <a href="index.php">
+                    <button type="submit" class="btn-2">Quay về trang chủ</button>
+                </a>
+        </div>
+    </div>
+</div>
+
+      
+
+
+
+
+
+
+
+
+<?php
+include('config/footer.php')
+
+?>
