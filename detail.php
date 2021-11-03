1cf37f02ae5b279a6dd2a2efcd7886c71de8fbb9
Hoangtantien <91014846+Hoangtantien@users.noreply.github.com>
2021-11-03 10:42:19 +0700

    Add files via upload

--- detail.php
+++ detail.php
@@ -1,59 +1,30 @@
 <?php
-session_start();
-include('config/header.php');
-include('config/conection.php');
-
+include('config/header.php')
 ?>
+<!--Navbar-->
+<section>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" alt="Logo">
+                </a>
 
-
+                <form class="d-flex">
+                    <a href="log_in.php">
+                        <button type="button" class="btn btn-primary me-3 ">Login</button>
+                    </a>
+                    <a href="log_out.php">
+                        <button type="button" class="btn btn-primary me-3 ">Logout</button>
+                    </a>
+                </form>
+               
+            </div>
+        </nav>
+    </section>
+    <!--Navbar-->
 
 <!--Tuyen du lieu-->
-<?php
-                if(isset($_GET['tour_id']))
-                $tour_id = $_GET['tour_id'];
-                $sql = "SELECT * FROM tour WHERE tour_id=$tour_id";
-                $result = mysqli_query($con,$sql);
-                //check xem bảng tour có đc kết nối hay ko
-                if(mysqli_num_rows($result)>0){
-                    while($row = mysqli_fetch_assoc($result)){
-                        $tour_id= $row['tour_id'];
-                        $tour_name=$row['tour_Name'];
-                        $tour_price=$row['tour_price'];
-                        $img=$row['img'];
-                        $tour_detail=$row['tour_detail'];
-                       
-                        
-                        //$tour_detail=$row['tour_detail'];
-?>
-
- <div class="container">
-     <h2> Chi tiết tour</h2>
-        <div id="tour-detail">
-            <div id= "tour-img">
-                <img src="<?php echo $img?>" alt="">
-
-            </div>
-    <div id="product-info">
-       <h4 class="tour_Name"><?php echo $tour_name ?></h4>
-       <p class="tour_detail"><?php echo $tour_detail?></p>
-       
-       <p class="tour_price " ><?php echo $tour_price?><span  style="color:red;" >VNĐ</span></p>
-       <a href="booking_tour.php">
-                        <button type="submit" class=" btn bg-danger text-light fw-bold">Đặt ngay</button>
-                    </a>
-
-    </div>
-        <div class="clear-both">
-        <p></p>
-        </div>
-        </div>
-
- </div>
- <?php
-            }
-        }
-
-?>
+ Tự code html theo web travel.com.vn/tourNDDNG116-011-301021XE-D/kham-pha-ngu-hanh-son-pho-co-hoi-an-tang-ve-xe-dien-nhom-4-khach.aspx
 <!--Tuyen du lieu-->
 
 <!--Footer-->
@@ -104,10 +75,7 @@ include('config/conection.php');
         </div>
     </section>
     <!--Footer-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="js/detail.js"></script>
+
 
 <?php
 include('config/footer.php')
